8e14a31461c151cfc0b23813f5ff773addb995d0
fhg6000 <fhg6000@163.com>
2020-08-07 00:10:34 +0800

    Add files via upload

---
+++ AR502H_Agent - 副本.c
@@ -0,0 +1,12 @@
+// AR502H_Agent.cpp : 定义控制台应用程序的入口点。
+#include "Global_Struct_Module.h"
+
+extern int Main_Module(int argc, _TCHAR* argv[]);
+int _tmain(int argc, _TCHAR* argv[])
+{
+	return Main_Module(argc,argv);
+	
+}
+
+
+
